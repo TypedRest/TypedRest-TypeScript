7894e72a0d3f3e2d5492ab639c68026c16ac52db
Bastian Eicher <bastian@eicher.net>
2020-08-31 13:22:47 +0200

    Improved header link extraction

--- links/HeaderLinkExtractor.ts
+++ links/HeaderLinkExtractor.ts
@@ -15,29 +15,41 @@ export class HeaderLinkExtractor implements LinkExtractor {
             ?? [];
     }
 
-
     private parseLink(value: string) {
-        const split = value.split(">");
-        const href = split[0].substring(1);
+        const split = this.split(value, ">");
+        const href = split.left.substring(1);
         let rel: string | undefined;
         let title: string | undefined;
         let templated = false;
 
-        split[1].match(/[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*"))/g)?.forEach(param => {
-            const paramSplit = param.split("=");
-            if (paramSplit[0] === "rel")
-                rel = paramSplit[1];
-            else if (paramSplit[0] === "title")
-                title = paramSplit[1];
-            else if (paramSplit[0] === "templated" && paramSplit[1])
+        split.right.match(/[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*"))/g)?.forEach(param => {
+            const paramSplit = this.split(param, "=");
+            if (paramSplit.left === "rel") {
+                rel = paramSplit.right;
+            } else if (paramSplit.left === "title") {
+                title = paramSplit.right;
+                if (title.startsWith('"') && title.endsWith('"')) {
+                    title = title.substring(1, title.length - 1);
+                }
+            } else if (paramSplit.left === "templated" && paramSplit.right === "true") {
                 templated = true;
+            }
         });
 
-        if (!href)
-            throw new Error("The link header is lacking the mandatory 'href' field.");
-        if (!rel)
-            throw new Error("The link header is lacking the mandatory 'rel' field.");
-
+        if (!rel) throw new Error("The link header is lacking the mandatory 'rel' field.");
         return new Link(rel, href, title, templated);
+    }
+
+    private split(str: string, separator: string): { left: string, right: string } {
+        const result = str.split(separator, 2);
+        return (result.length === 2)
+            ? {
+                left: result[0],
+                right: result[1] + str.substr(result.join(separator).length)
+            }
+            : {
+                left: result[0],
+                right: ''
+            };
     }
 }
